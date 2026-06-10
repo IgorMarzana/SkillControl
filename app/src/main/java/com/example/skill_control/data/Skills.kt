6a75f57474a415e6c6ca8ae4a0bd49cc1b704a58
IgorMarzana <igormarzana@gmail.com>
2026-06-09 21:37:03 -0400

    base de datos formada

--- app/src/main/java/com/example/skill_control/data/Skills.kt
+++ app/src/main/java/com/example/skill_control/data/Skills.kt
@@ -2,16 +2,52 @@ package com.example.skill_control.data
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 data class Skills(val id: Int, val nombre: String,var nivel: Int)
 
 object RepositorioSkills{
 
     val listaHabilidades = mutableStateListOf(
+
         Skills(1, "Game", 1),
         Skills(2, "Frame", 1),
         Skills(3, "Looks", 1),
         Skills(4, "Money", 1)
+
     )
+
+    fun dameNombreHabilidad(id: Int): String?{
+
+        listaHabilidades.forEach {
+
+            if(it.id == id){
+
+                return it.nombre
+
+            }
+
+        }
+
+        return null
+
+    }
+
+    fun estableceNivel(id: Int, nivel: Int){
+
+        listaHabilidades.forEachIndexed   { indice, skills ->
+
+            if(skills.id == id ){
+
+                listaHabilidades[indice] = Skills(id, skills.nombre, nivel)
+
+            }
+
+        }
+        
+    }
+
+
+
 
 }
